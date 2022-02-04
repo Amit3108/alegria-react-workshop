e928142ca5b7ddf49605b244e7b311ed49de6ce0
Praful kumar <67057379+Prafulkumar0512@users.noreply.github.com>
2022-02-05 02:07:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import React, { useState, useEffect } from "react";
 function App() {
   const [value, setValue] = useState(0);
   const [count, setCount] = useState(0);
+  const [low, setLow] = useState(0)
   useEffect(() => {
     console.log(`Current Value = ${value} \n Button Clicks= ${count}`);
-  });
+  }, [low]);
   return (
     <div>
       <p>You clicked {count} times</p>
       <p>Current Value {value}</p>
       <button onClick={() => {
-        setValue(value + 1) 
-        setCount(count+1)
+        setValue(value + 1)
+        setCount(count + 1)
       }}>
         Increment +
       </button>
       <button onClick={() => {
-        setValue(value - 1) 
+        setValue(value - 1)
         setCount(count + 1)
+        setLow(low + 1)
       }}>
         Decrement -
       </button>
